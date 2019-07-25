f3ad1beff805c00aacdbf3fb2ce136fef0c15961
Ziao Wang <zw2498@columbia.edu>
2019-07-25 16:58:40 -0400

    Updates

--- precache-manifest.2498db00c3d6709b96b804f9bd6ab1e4.js
+++ precache-manifest.2498db00c3d6709b96b804f9bd6ab1e4.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3a7051caf9df4cd3aa65306b9f708548",
+    "revision": "fc6e786d6f5b1d2f1564b040ef53ce36",
     "url": "/index.html"
   },
   {
-    "revision": "4b623e8593c9ac990379",
+    "revision": "f1c63847b609e64d612c",
     "url": "/static/css/main.2cce8147.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.b41502e9.chunk.js"
   },
   {
-    "revision": "4b623e8593c9ac990379",
-    "url": "/static/js/main.28647029.chunk.js"
+    "revision": "f1c63847b609e64d612c",
+    "url": "/static/js/main.981416d3.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.2498db00c3d6709b96b804f9bd6ab1e4.js"
 );
 
 self.addEventListener('message', (event) => {
